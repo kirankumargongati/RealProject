4edb89bb2bcb3676618177f3507d24bdd79c8d21
kiran <kirankumargogati325@gmail.com>
2025-06-01 14:15:37 +0530

    Second Commit

--- src/test/java/SeleniumPractice/GetTableText.java
+++ src/test/java/SeleniumPractice/GetTableText.java
@@ -11,38 +11,53 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GetTableText {
-	WebElement driver;
-	
-	public GetTableText(WebElement driver) {
-		// TODO Auto-generated constructor stub
-		driver=this.driver;
-	}
+import dev.failsafe.internal.util.Assert;
 
+public class GetTableText {
+	public  WebDriver driver;
+	public GetTableText(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	
 	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver()	;
+		GetTableText obj=new GetTableText(driver);
+		obj.GetTextQA();
 		// TODO Auto-generated method stub
-		
-  WebDriver driver=new ChromeDriver();
-  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(300));
-  driver.get("https://testpages.eviltester.com/styled/index.html");
-  driver.findElement(By.xpath("tablestest")).click();
-   WebElement table=driver.findElement(By.xpath("//table[@id=\"mytable\"]/tbody"));
-  List<WebElement> rows=table.findElements(By.tagName("tr"));
-  for (WebElement row : rows) {
-	  List<WebElement> cells=table.findElements(By.tagName("td"));
-	  for (WebElement cell : cells) {
-		  System.out.println(cell.getText());
-		
+//	public void QAfoxGetTable() {
+//		WebDriver driver=new ChromeDriver();
+//		driver.get("https://omayo.blogspot.com/");
+//		WebElement ele=driver.findElement(By.id("table1"));
+//		List<WebElement> row=ele.findElements(By.tagName("tr"));
+//		//List<WebElement> column=ele.findElements(By.tagName("th"));
+//		for (WebElement rows : row) {
+//			List<WebElement> cell=rows.findElements(By.tagName("td"));
+//			for(WebElement cells:cell) {
+//				System.out.println(cells.getText());
+//			}
+//		}
+//	}
 	}
-	  System.out.println();
-	  
-	  
-	
-}
-  driver.close();
+	public  void GetTextQA() {
+	driver.get("https://omayo.blogspot.com/");
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	driver.manage().window().maximize();
+	WebElement table=driver.findElement(By.id("table1"));
+	List<WebElement> row=table.findElements(By.tagName("tr"));
+	for(WebElement rows:row) {
+		List<WebElement> cell=rows.findElements(By.tagName("td"));
+		for(WebElement cells:cell) {
+			System.out.println(cells.getText());
+			String name=cells.getText();
+			if(name.equals("Manish")) {
+				System.out.println("expected condition matched");
+			}
+		
+		}
 	}
-	
-	
+	driver.close();
+	}
 	
 
 }
